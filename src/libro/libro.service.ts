05153ea05dfbb0d8f8cec2dc1ea17fafefc9e30c
enriquejoau <55159544+enriquejoau@users.noreply.github.com>
2024-09-12 08:12:43 -0500

    Update libro.service.ts

--- src/libro/libro.service.ts
+++ src/libro/libro.service.ts
@@ -13,18 +13,15 @@ export class LibroService {
     private readonly libroModel: typeof Libro,
   ) {}
 
-  // Crear un nuevo libro
+
   async create(createLibroDto: CreateLibroDto): Promise<Libro> {
-    // Desestructuramos los campos de CreateLibroDto
     const { titulo, fecha_publicacion, autorId, categorias } = createLibroDto;
 
-    // Validar que el autor exista
     const autor = await Autor.findByPk(autorId);
     if (!autor) {
       throw new NotFoundException('Autor no encontrado');
     }
 
-    // Validar que las categorías existan
     const categoriasValidas = await Categoria.findAll({
       where: { id: categorias },
     });
@@ -32,31 +29,27 @@ export class LibroService {
       throw new BadRequestException('Una o más categorías no existen');
     }
 
-    // Crear el libro excluyendo las categorías
     const libro = await this.libroModel.create({
       titulo,
       fecha_publicacion,
       autorId,
     });
 
-    // Asociar las categorías al libro creado
     await libro.$set('categorias', categorias);
 
     return libro;
   }
   
 
-  // Obtener todos los libros con paginación
   async findAll(page: number = 1, limit: number = 10): Promise<Libro[]> {
     const offset = (page - 1) * limit;
     return this.libroModel.findAll({
-      include: [Autor, Categoria], // Incluir autor y categorías
+      include: [Autor, Categoria], 
       offset,
       limit,
     });
   }
 
-  // Obtener un libro por ID
   async findOne(id: string): Promise<Libro> {
     const libro = await this.libroModel.findByPk(id, { include: [Autor, Categoria] });
     if (!libro) {
@@ -65,12 +58,10 @@ export class LibroService {
     return libro;
   }
 
-  // Actualizar un libro
   async update(id: string, updateLibroDto: UpdateLibroDto): Promise<Libro> {
     const libro = await this.findOne(id);
     const { categorias } = updateLibroDto;
 
-    // Validar que las categorías existan
     if (categorias) {
       const categoriasValidas = await Categoria.findAll({
         where: { id: categorias },
@@ -84,7 +75,6 @@ export class LibroService {
     return libro.update(updateLibroDto);
   }
 
-  // Eliminar un libro
   async delete(id: string): Promise<void> {
     const libro = await this.findOne(id);
     await libro.destroy();
